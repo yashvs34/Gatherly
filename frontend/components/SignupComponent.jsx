867ee21d21db6df5f57d64da02e2525ae932ed05
yashvs34 <yashvardhansingh232@gmail.com>
2025-10-01 11:38:09 +0530

    Made html structure of signup component

--- frontend/components/SignupComponent.jsx
+++ frontend/components/SignupComponent.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 
 function SignupComponent ()
 {
-
+    const [clicked, setClicked] = useState(false);
+    
+    return (
+        <div>
+            <div>Signup</div>
+            <div>Create your account to get started!</div>
+            <div><input type="text" placeholder="Enter email" /></div>
+            <div><input type="text" placeholder="Enter your firstname" /></div>
+            <div><input type="text" placeholder="Enter your lastname" /></div>
+            <div><input type="text" placeholder="choose a username" /></div>
+            <div><input type={clicked ? "text" : "password"} placeholder="Enter password" /></div>
+            <div><input type="checkbox" checked={clicked} onChange={(e) => {
+                setClicked(e.target.checked);
+            }} /> Show password</div>
+            <div>Signup</div>
+        </div>
+    )
 }
 
 export default SignupComponent;
